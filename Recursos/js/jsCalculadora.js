65c8be7c6d2cff7c3fbd7471603493bea273c398
Gonzagut99 <102807908+Gonzagut99@users.noreply.github.com>
2022-05-03 19:59:30 -0500

    update

--- Recursos/js/jsCalculadora.js
+++ Recursos/js/jsCalculadora.js
@@ -1,13 +1,49 @@
 
+/*function fnSumar(){
+    var NroUno = parseFloat(document.getElementById("numerouno").value)
+    var NroDos = parseFloat(document.getElementById("numerodos").value)
+    document.getElementById("rFinal").innerHTML= NroUno+NroDos
+}*/
+function fnRestar(){
+    var NroUno = parseFloat(document.getElementById("numerouno").value)
+    var NroDos = parseFloat(document.getElementById("numerodos").value)
+    document.getElementById("rFinal").innerHTML= NroUno-NroDos
+}
+function fnMultiplicar(){
+    var NroUno = parseFloat(document.getElementById("numerouno").value)
+    var NroDos = parseFloat(document.getElementById("numerodos").value)
+    document.getElementById("rFinal").innerHTML= NroUno*NroDos
+}
+function fnDividir(){
+    var NroUno = parseFloat(document.getElementById("numerouno").value)
+    var NroDos = parseFloat(document.getElementById("numerodos").value)
+    /*parseFloat($("#numerodos").val())*/
+   document.getElementById("rFinal").innerHTML= NroUno/NroDos
+}
+    
+function fnCalcular(){  
+    
+    //llamando objetos de radio
+    var NroUno = parseFloat(document.getElementById("numerouno").value)
+    /*var NroDos = parseInt(document.getElementById("numerodos").value)*/
+    /*var NroUno = parseFloat($("#numerouno").val());*/
+    var NroDos = parseFloat($("#numerodos").val())
+    var vChecked = document.querySelector('input[name="flexRadio"]:checked').value
+    
+    if(vChecked=="RadioSum"){
+        document.getElementById("rFinal").innerHTML= NroUno+NroDos;
+    }
+    
+    if(vChecked=="RadioRest"){
+        fnRestar();
+    }
 
-function fnCalcular(){
+    if(vChecked=="RadioMult"){
+        fnMultiplicar();
+    }
 
-    var NroUno = "10";
-
-    var AuxNro = parseFloat(NroUno)
-    var NroDos = 20;
-
-
-    return AuxNro + NroDos;
-
+    if(vChecked=="RadioDiv"){
+        fnDividir();
+    }
+    
 }
